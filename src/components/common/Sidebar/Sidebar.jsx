5a5418203dbde6e78176452cc2a90afde64c1df1
PawlDani <pawldani@gmail.com>
2024-02-04 17:02:41 +0100

    Removed header, changed sidebar layout, moved root styling to main.scss, changed colors

--- src/components/common/Sidebar/Sidebar.jsx
+++ src/components/common/Sidebar/Sidebar.jsx
@@ -1,20 +1,33 @@
+import maxLogo from '/src/assets/images/max-logo.png'; // Ścieżka do logo
+
+// Komponent paska bocznego
 const Sidebar = ({ activeTab, setActiveTab }) => {
+  // Przyjmuje dwa propsy: activeTab oraz setActiveTab. setActiveTab to funkcja, ktora zmienia activeTab
+  // Definicja zakładek w pasku bocznym
   const tabs = [
     { name: 'Dashboard', id: 'Dashboard' },
     { name: 'Wordsets', id: 'Wordsets' },
     { name: 'Progress', id: 'Progress' },
     { name: 'How To', id: 'HowTo' },
-    // Add more tabs here as needed
+
+    // Wiecej zakladek bede dodawal tutaj
   ];
 
+  // Renderowanie paska bocznego
   return (
-    <div className="side_bar">
-      <div className="side_bar_bottom">
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <div className="sidebar_logo">
+          <img src={maxLogo} alt="logo" />
+        </div>
+      </div>
+      <div className="sidebar_bottom">
         <ul>
           {tabs.map((tab) => (
             <li key={tab.id} className={activeTab === tab.id ? 'active' : ''} onClick={() => setActiveTab(tab.id)}>
+              {/* Klikniecie na zakladke zmienia activeTab na id tej zakladki */}
               <a href="#">
-                <span className="icon"></span> {/* Add icon logic here */}
+                <span className="icon"></span> {/* Miejsce na ikonę */}
                 <span className="title">{tab.name}</span>
               </a>
               {activeTab === tab.id && (

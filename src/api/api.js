c4fea766f74fa80b0d994b3704f0663d2b4e3a03
PawlDani <pawldani@gmail.com>
2024-02-23 10:17:40 +0100

    Introduced progress tracking for translation game using server

--- src/api/api.js
+++ src/api/api.js
@@ -4,29 +4,24 @@ import supabase from './supabaseClient';
 export const fetchWordsets = async () => {
   // Wykonuje zapytanie do bazy danych Supabase
   const { data: wordsets, error } = await supabase.from('finaltest').select('*');
-
   // Loguje błąd, jeśli wystąpi podczas pobierania danych
   if (error) {
     console.error('Błąd podczas pobierania zestawów słów:', error);
     return null; // Zwraca null jako sygnał niepowodzenia operacji
   }
-
   // Zwraca pobrane zestawy słów, jeśli operacja się powiedzie
   return wordsets;
 };
 
 // Pobiera terminy z bazy danych na podstawie kategorii
 export const fetchTermsByCategory = async (category) => {
-  // Wykonuje zapytanie do bazy danych Supabase z filtrem dla konkretnej kategorii
   const { data: terms, error } = await supabase.from('finaltest').select('*').eq('category', category);
 
-  // Loguje błąd, jeśli wystąpi podczas pobierania danych dla danej kategorii
   if (error) {
     console.error(`Błąd podczas pobierania terminów dla kategorii ${category}:`, error);
-    return []; // Zwraca pustą tablicę jako sygnał niepowodzenia operacji
+    return [];
   }
 
-  // Zwraca pobrane terminy, jeśli operacja się powiedzie
   return terms;
 };
 
@@ -63,4 +58,95 @@ export const fetchFavorites = async (userId) => {
     return [];
   }
   return data.map((fav) => fav.category);
+};
+
+// Pobiera postęp użytkownika dla określonej kategorii
+export const fetchUserProgress = async (userId, category) => {
+  try {
+    const { data, error } = await supabase
+      .from('user_progress')
+      .select('*')
+      .eq('user_id', userId)
+      .eq('category', category);
+
+    if (error) throw error;
+
+    // Zwraca dane postępu użytkownika lub pustą tablicę, jeśli nie ma danych
+    return { data: data || [], error: null };
+  } catch (error) {
+    console.error('Error fetching user progress:', error);
+    return { data: null, error };
+  }
+};
+
+// Aktualizuje postęp użytkownika dla określonej kategorii i terminu
+export const updateUserProgress = async (userId, category, termId, incrementCorrectAttempts) => {
+  try {
+    // Pobiera aktualny postęp użytkownika dla określonej kategorii i terminu
+    const { data: currentProgress, error: fetchError } = await supabase
+      .from('user_progress')
+      .select('correct_attempts')
+      .eq('user_id', userId)
+      .eq('category', category)
+      .eq('term_id', termId)
+      .maybeSingle();
+
+    if (fetchError) {
+      throw fetchError;
+    }
+
+    let newCorrectAttempts = currentProgress ? currentProgress.correct_attempts : 0;
+    let completed = currentProgress ? currentProgress.completed : false;
+
+    // Inkrementuje liczbę poprawnych prób, jeśli jest to wymagane
+    if (incrementCorrectAttempts) {
+      newCorrectAttempts++;
+    }
+
+    // Oznacza termin jako ukończony, jeśli użytkownik uzyskał co najmniej 3 poprawne próby
+    if (newCorrectAttempts >= 3 && !completed) {
+      completed = true;
+    }
+
+    // Aktualizuje postęp użytkownika w bazie danych
+    const { error } = await supabase.from('user_progress').upsert(
+      {
+        user_id: userId,
+        category: category,
+        term_id: termId,
+        correct_attempts: newCorrectAttempts,
+        completed: completed,
+        last_attempt: new Date().toISOString(),
+      },
+      {
+        onConflict: 'user_id, category, term_id',
+      }
+    );
+
+    if (error) {
+      throw error;
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.error('Error updating user progress:', error);
+    return { error };
+  }
+};
+
+// Resetuje postęp użytkownika dla określonej kategorii
+export const resetUserProgress = async (userId, category) => {
+  try {
+    const { error } = await supabase
+      .from('user_progress')
+      .update({ correct_attempts: 0, completed: false })
+      .match({ user_id: userId, category: category });
+
+    if (error) throw error;
+
+    return { success: true };
+  } catch (error) {
+    console.error('Error resetting user progress:', error);
+    return { error };
+  }
 };
--- src/components/Exercises/Translation/TranslationComponent.jsx
+++ src/components/Exercises/Translation/TranslationComponent.jsx
@@ -2,105 +2,140 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from '/src/components/Exercises/Modal/modal';
 import './Translation.scss';
+import { updateUserProgress, fetchUserProgress, resetUserProgress } from '/src/api/api';
+import { useAuth } from '/src/hooks/AuthProvider';
 
 const TranslationComponent = ({ terms, onChangeGame, category }) => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [userInput, setUserInput] = useState('');
   const [isCorrect, setIsCorrect] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
-  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [correctFirstAttempts, setCorrectFirstAttempts] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [termProgress, setTermProgress] = useState(
+    new Array(terms.length).fill({ attempted: false, correctFirstAttempt: false })
+  );
+  const navigate = useNavigate();
 
-  // Definicja klucza stanu gry dla danej kategorii
-  const gameStateKey = `translationGameState-${category}`;
+  const { user } = useAuth();
+  const userId = user?.id;
 
+  // Pobieranie postępu użytkownika z bazy danych
   useEffect(() => {
-    // Pobranie stanu gry z localStorage
-    const savedState = JSON.parse(localStorage.getItem(gameStateKey));
-    if (savedState) {
-      setCurrentCardIndex(savedState.currentCardIndex);
-      setCorrectAnswersCount(savedState.correctAnswersCount);
-      setShowModal(savedState.correctAnswersCount === terms.length);
+    if (userId) {
+      const initTermProgress = async () => {
+        for (let i = 0; i < terms.length; i++) {
+          const { data, error } = await fetchUserProgress(userId, terms[i].category);
+          if (!error && data) {
+            setTermProgress((prev) =>
+              prev.map((tp, index) => (index === i ? { ...tp, correctFirstAttempt: data.correct_attempts >= 3 } : tp))
+            );
+          }
+        }
+      };
+      initTermProgress();
     }
-  }, [terms, category, gameStateKey]);
+  }, [terms, userId]);
 
-  // Zapisanie stanu gry do localStorage
-  const saveGameState = useCallback(() => {
-    const gameState = { currentCardIndex, correctAnswersCount };
-    localStorage.setItem(gameStateKey, JSON.stringify(gameState));
-  }, [currentCardIndex, correctAnswersCount, gameStateKey]);
-
-  // Czyszczenie stanu gry z localStorage
-  const clearGameState = useCallback(() => {
-    localStorage.removeItem(gameStateKey);
-  }, [gameStateKey]);
-
-  // Funkcja wywoływana przy sprawdzaniu odpowiedzi
-  const handleCheckAnswer = useCallback(() => {
-    const correct = userInput.trim().toLowerCase() === terms[currentCardIndex].term.toLowerCase();
-    setIsCorrect(correct); // Ustawienie stanu poprawności odpowiedzi
-    console.log(correct ? 'Correct answer' : 'Incorrect answer');
-    if (correct) {
-      saveGameState(); // Zapisanie stanu gry do localStorage po poprawnej odpowiedzi
-    } else {
-      setErrorMessage('');
+  // Funkcja sprawdzająca, czy wszystkie terminy zostały ukończone
+  const checkAllTermsCompleted = useCallback(async () => {
+    const { data, error } = await fetchUserProgress(userId, category);
+    if (error) {
+      console.error('Error fetching all user progress:', error);
+      return false;
     }
-  }, [userInput, terms, currentCardIndex, saveGameState]);
 
-  // Funkcja wywoływana przy przejściu do następnej karty
-  const handleNextCard = useCallback(() => {
+    return data && data.every((p) => p.completed);
+  }, [userId, category]);
+
+  const handleCheckAnswer = useCallback(async () => {
     const correct = userInput.trim().toLowerCase() === terms[currentCardIndex].term.toLowerCase();
+    setIsCorrect(correct);
     if (correct) {
-      const newCorrectAnswersCount = correctAnswersCount + 1;
-      setCorrectAnswersCount(newCorrectAnswersCount); // Zwiększenie liczby poprawnych odpowiedzi
-      if (newCorrectAnswersCount === terms.length) {
-        setShowModal(true); // Pokaż modal, gdy wszystkie terminy są zakończone
+      if (!termProgress[currentCardIndex].attempted) {
+        const newCorrectFirstAttempts = [...correctFirstAttempts, terms[currentCardIndex].id];
+        setCorrectFirstAttempts(newCorrectFirstAttempts);
+        if (userId) {
+          await updateUserProgress(userId, category, terms[currentCardIndex].id, true);
+
+          const allCompleted = await checkAllTermsCompleted();
+          if (allCompleted) {
+            setShowModal(true);
+          }
+        } else {
+          console.error('No user ID available, cannot update progress.');
+        }
       }
-      setCurrentCardIndex((prevIndex) => (prevIndex + 1) % terms.length); // Przejście do następnej karty
-      setUserInput(''); // Czyszczenie pola wprowadzania
-      setIsCorrect(null); // Reset stanu dla następnej karty
-      setErrorMessage(''); // Reset komunikatu o błędzie przy przejściu do następnej karty
-      console.log('Moving to next card:', currentCardIndex + 1);
-
-      saveGameState(); // Zapisanie stanu gry do localStorage po przejściu do następnej karty
     } else {
-      setErrorMessage('Niepoprawnie, proszę wpisać poprawną odpowiedź, aby kontynuować.'); // Komunikat o błędzie przy niepoprawnej odpowiedzi (nie można przejść dalej)
-      console.log('Incorrect answer');
+      setErrorMessage('Niepoprawnie, proszę wpisać poprawną odpowiedź, aby kontynuować.');
     }
-  }, [userInput, terms, currentCardIndex, correctAnswersCount, saveGameState]);
+    setTermProgress((prev) => prev.map((tp, index) => (index === currentCardIndex ? { ...tp, attempted: true } : tp)));
+  }, [
+    userInput,
+    terms,
+    currentCardIndex,
+    termProgress,
+    userId,
+    category,
+    correctFirstAttempts,
+    checkAllTermsCompleted,
+  ]);
 
-  // Obsługa zdarzeń klawiatury dla całego komponentu
+  const handleNextCard = useCallback(() => {
+    const newTermProgress = [...termProgress];
+    newTermProgress[currentCardIndex].attempted = false;
+
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % terms.length);
+    setUserInput('');
+    setIsCorrect(null);
+    setErrorMessage('');
+
+    if (currentCardIndex + 1 === terms.length) {
+      const allAttempted = termProgress.every((tp) => tp.attempted);
+      if (allAttempted) {
+        const fullyLearned = termProgress.filter((tp) => tp.correctFirstAttempt).length;
+        if (fullyLearned === terms.length) {
+          setShowModal(true);
+        } else {
+          // Prepare for the next round
+          setTermProgress(newTermProgress.map((tp) => ({ ...tp, attempted: false })));
+          setCurrentCardIndex(0);
+        }
+      }
+    }
+  }, [currentCardIndex, terms.length, termProgress]);
+
   const handleKeyPress = useCallback(
     (event) => {
       if (event.key === 'Enter') {
-        isCorrect === null ? handleCheckAnswer() : handleNextCard(); // Logika decydująca, czy sprawdzić odpowiedź, czy przejść dalej
+        isCorrect === null ? handleCheckAnswer() : handleNextCard();
       }
     },
     [isCorrect, handleCheckAnswer, handleNextCard]
   );
 
-  // Akcje dla modalu końca gry
-  const navigate = useNavigate();
-
   const modalActions = [
     {
       label: 'Rozpocznij ponownie',
       className: 'restart-button',
-      onClick: () => {
-        clearGameState(); // Czyszczenie stanu gry z localStorage
-        setCorrectAnswersCount(0); // Reset liczby poprawnych odpowiedzi
-        setCurrentCardIndex(0); // Reset indeksu bieżącej karty
-        setShowModal(false); // Ukrycie modalu
-        onChangeGame('translation'); // Restart gry
+      onClick: async () => {
+        const { error } = await resetUserProgress(userId, category);
+        if (error) {
+          console.error('Could not reset progress:', error);
+        } else {
+          setCorrectFirstAttempts([]);
+          setCurrentCardIndex(0);
+          setShowModal(false);
+          setTermProgress(new Array(terms.length).fill({ attempted: false, correctFirstAttempt: false }));
+          onChangeGame('translation');
+        }
       },
       icon: 'fa-solid fa-repeat',
     },
     {
       label: 'Przejdź do innego zestawu',
       className: 'switch-button',
-      onClick: () => {
-        navigate('/wordsets');
-      },
+      onClick: () => navigate('/wordsets'),
       icon: 'fa-solid fa-forward-step',
     },
   ];
@@ -116,18 +151,14 @@ const TranslationComponent = ({ terms, onChangeGame, category }) => {
         />
       )}
       <div className="translation-game-container" onKeyDown={handleKeyPress} tabIndex="0">
-        {errorMessage && <div className="error-message">{errorMessage}</div>} {/* Wyświetlanie komunikatu o błędzie */}
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
         {isCorrect !== null && (
-          <h2 className={`answer-header ${isCorrect ? 'correct' : 'incorrect'}`}>
-            {isCorrect ? 'Dobrze' : 'Źle'} {/* Nagłówek informujący o poprawności odpowiedzi */}
-          </h2>
+          <h2 className={`answer-header ${isCorrect ? 'correct' : 'incorrect'}`}>{isCorrect ? 'Dobrze' : 'Źle'}</h2>
         )}
         <div className={`translation-card ${isCorrect ? 'correct' : isCorrect === false ? 'incorrect' : ''}`}>
           <div className="term">{terms[currentCardIndex].definition}</div>
           {isCorrect === false && (
             <>
-              {' '}
-              {/* Wyświetlanie informacji zwrotnej tylko dla niepoprawnych odpowiedzi */}
               <div className="your-answer">Twoja odpowiedź: {userInput}</div>
               <div className="right-answer">Prawidłowa odpowiedź: {terms[currentCardIndex].term}</div>
             </>
@@ -136,17 +167,17 @@ const TranslationComponent = ({ terms, onChangeGame, category }) => {
         <input
           type="text"
           value={userInput}
-          onChange={(e) => setUserInput(e.target.value)} // Aktualizacja wprowadzonego tekstu
-          placeholder={isCorrect === false ? 'Przepisz odpowiedź' : 'Wpisz tłumaczenie'} // Zmiana placeholdera w zależności od stanu
-          autoFocus // Automatyczne ustawienie fokusu na polu wprowadzania
+          onChange={(e) => setUserInput(e.target.value)}
+          placeholder={isCorrect === false ? 'Przepisz odpowiedź' : 'Wpisz tłumaczenie'}
+          autoFocus
         />
         <div className="button-container">
           <button className="check-button" onClick={handleCheckAnswer} disabled={isCorrect !== null}>
-            Sprawdź {/* Przycisk do sprawdzania odpowiedzi */}
+            Sprawdź
           </button>
           {isCorrect !== null && (
             <button className="next-button" onClick={handleNextCard}>
-              Następna karta {/* Przycisk do przejścia do następnej karty */}
+              Następna karta
             </button>
           )}
         </div>
